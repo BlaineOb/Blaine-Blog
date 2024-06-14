c05bca57786c95a69db50382d90d3a40861c3d79
BlaineOb <blnob567@gmail.com>
2024-06-14 13:46:22 -0400

    Initial Blog Post

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+	import '../app.css'
+</script>
+
+<style>
+	h1 {
+			margin-top: 20px;
+			font-size: 2.5rem;
+	}
+
+	a {
+			text-decoration: none;
+			color: white;
+			font-size: 1.25rem;
+			padding-left: 10px;
+	}
+
+	a:hover{
+      color: darkgray;
+  }
+</style>
+
+<h1>
+	Blaine's Blog <a href="/">Home</a> <a href="/links">Links</a>
+</h1>
+
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,15 @@
-<h1>Blaine's Blog</h1>
-<h2>Blog Post #1</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<style>
+	a {
+      margin-top: 20px;
+			display: inline-block;
+			color: hsl(240, 52%, 86%);
+			font-size: 1.5rem;
+      text-decoration: none;
+	}
+
+  a:hover{
+      color: darkgray;
+  }
+</style>
+
+<a href="/post-1">Practice And Performance Are Opposites</a>
