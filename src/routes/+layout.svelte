da9919228a1eaa509dc1d33acb585d5bbcd0bdc8
BlaineOb <blnob567@gmail.com>
2025-04-29 21:09:35 -0400

    Update Styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,32 +1,6 @@
 <script>
-	import '../app.css'
+	import '../app.css';
 </script>
-
-<style>
-	h1 {
-			margin-top: 20px;
-			font-size: 2.5rem;
-	}
-
-	a {
-			text-decoration: none;
-			color: white;
-			font-size: 1.25rem;
-	}
-
-	a:hover{
-      color: darkgray;
-  }
-
-	div {
-      margin-left: 20%;
-	}
-
-	.title {
-      margin-top: 20px;
-      font-size: 2.5rem;
-	}
-</style>
 
 <div>
 	<h1>
@@ -34,3 +8,29 @@
 	</h1>
 	<slot />
 </div>
+
+<style>
+	h1 {
+		margin-top: 20px;
+		font-size: 2.5rem;
+	}
+
+	a {
+		text-decoration: none;
+		color: #1db954;
+		font-size: 1.25rem;
+	}
+
+	a:hover {
+		color: darkgray;
+	}
+
+	div {
+		margin-left: 20%;
+	}
+
+	.title {
+		margin-top: 20px;
+		font-size: 2.5rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,27 @@
-<style>
-	a {
-      margin-top: 20px;
-			display: inline-block;
-			color: hsl(240, 52%, 86%);
-			font-size: 1.5rem;
-      text-decoration: none;
-	}
-
-  a:hover{
-      color: darkgray;
-  }
-
-	p {
-			font-style: italic;
-	}
-
-	div {
-			padding-bottom: 20px;
-			height: 80%;
-	}
-</style>
-
 <div>
 	<a href="/post-1">Practice and Performance are Opposites</a>
 	<p>6/14/2024</p>
 </div>
 
+<style>
+	a {
+		margin-top: 20px;
+		display: inline-block;
+		color: #1db954;
+		font-size: 1.5rem;
+		text-decoration: none;
+	}
+
+	a:hover {
+		color: darkgray;
+	}
+
+	p {
+		font-style: italic;
+	}
+
+	div {
+		padding-bottom: 20px;
+		height: 80%;
+	}
+</style>
